ce2de669ca2002a25a46cf6598a6d462c185fe9e
realProject <tolqinmirsaliyev@gmail.com>
2024-05-11 11:44:12 +0500

    sss

--- index.js
+++ index.js
@@ -15,12 +15,7 @@ const path = require('path')
 
 const app = express();
 app.use(express.json());
-app.use(cors(
-  {
-      origin: "http://localhost:5173",
-      credentials: true
-  }
-))
+app.use(cors())
 
 
 
