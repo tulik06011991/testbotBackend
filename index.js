b522292acca722e97fc913f196d80c99b9398287
realProject <tolqinmirsaliyev@gmail.com>
2024-05-11 12:21:33 +0500

    hhhff

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    
+      origin: "http://localhost:5173" || "https://testbotbackend-8.onrender.com",
       credentials: true
   }
 ))
