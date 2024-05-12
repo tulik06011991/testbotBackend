aae7b654b4f998728ba9e00026448b6fedc120e2
tulik <tolqinmirsaliyev@gmail.com>
2024-05-12 14:02:46 -0500

    nnbbnbnb

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ app.use((req, res, next) => {
 app.use(cors())
 
 
+// }));app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST' ,'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   allowedHeaders: '*'
+// }));
 
 
 app.use('/auth', auth);
