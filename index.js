1a7ed99c8b8095a3baa428a5ac43dada81a48ddc
realProject <tolqinmirsaliyev@gmail.com>
2024-05-14 10:11:35 +0500

    ssssdsd

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const path = require('path')
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://mirsaliyev-tulqin04.netlify.app/',
+  origin: 'https://mirsaliyev-tulqin06.netlify.app/',
   credentials: true
 }));
 
